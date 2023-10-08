f9f10edd8c6860afc6bc8a43fcf308982991feb1
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-10-08 21:45:32 +0300

    front end done (very very ugly)

--- public/game.js
+++ public/game.js
@@ -1,50 +1,60 @@
 let app = new PIXI.Application({
-    width: 450,
-    height: 800,
-    backgroundColor: 0x31B9B3,
+    width: 550,
+    height: 810,
     antialias: true
 });
 
 document.body.appendChild(app.view);
 
-const upButtonPath = "sprites/blueUp.png";
-const upButton = new ozgurButton(upButtonPath, 2, app.screen.width/2, app.screen.height/8);
-app.stage.addChild(upButton.pixiObj);
+const blueUpButtonPath = "sprites/blueUp.png";
+const blueDownButtonPath = "sprites/blueDown.png";
+const blueBoxPath = "sprites/blueBox.png";
+const blueBoxFlatPath = "sprites/blueBoxFlat.png";
+const blueButtonPath = "sprites/blueButton.png";
+const blueButtonPressedPath = "sprites/blueButtonPressed.png";
 
-const upButtonData = { id: "0", credits: 100 };
+const slotMachinePath = "sprites/slotMachine.png";
+const blueSlotBackgroundPath = "sprites/slotBackgroundBlue.png";
 
-upButton.pixiObj.on("click", () => {
-    fetch("/UpButton", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(upButtonData),
-    }).then(() => {
-      console.log("Up Button pressed");
-      console.log(JSON.stringify(upButtonData));
-    });
-});
+const slotMachineSprite = PIXI.Sprite.from(slotMachinePath);
+app.stage.addChild(slotMachineSprite);
 
-const downButtonPath = "sprites/blueDown.png";
-const downButton = new ozgurButton(downButtonPath, 2, app.screen.width/2, app.screen.height/8*7);
-app.stage.addChild(downButton.pixiObj);
+const playButton = new ozgurButton(blueButtonPath, 1, 275, 620);
+app.stage.addChild(playButton.pixiObj);
 
-downButton.pixiObj.on("click", () => {
-  fetch("/DownButton", {
-    method: "POST",
-  }).then(() => {
-    console.log("Down Button pressed");
-  });
-});
+const increaseBetButton = new ozgurButton(blueUpButtonPath, 1, 400, 675);
+app.stage.addChild(increaseBetButton.pixiObj);
 
-const style = new PIXI.TextStyle({
-  fill: 0xffffff,
-  fontSize: 48,
-});
+const decreaseBetButton = new ozgurButton(blueDownButtonPath, 1, 150, 675);
+app.stage.addChild(decreaseBetButton.pixiObj);
 
-const text = new PIXI.Text("text", style);
-text.anchor.set(0.5);
-text.x = app.screen.width / 2;
-text.y = app.screen.height / 2;
-app.stage.addChild(text);
+const betBox = PIXI.Sprite.from(blueBoxFlatPath);
+betBox.anchor.set(0.5, 0.5);
+betBox.x = 275;
+betBox.y = 675;
+app.stage.addChild(betBox);
+
+const creditsBox = PIXI.Sprite.from(blueBoxFlatPath);
+creditsBox.anchor.set(0.5, 0.5);
+creditsBox.scale.set(2, 1);
+creditsBox.x = 275;
+creditsBox.y = 750;
+app.stage.addChild(creditsBox);
+
+const slotBackgroundLeft = PIXI.Sprite.from(blueSlotBackgroundPath);
+slotBackgroundLeft.anchor.set(0.5, 0.5);
+slotBackgroundLeft.x = 145;
+slotBackgroundLeft.y = 350;
+app.stage.addChild(slotBackgroundLeft);
+
+const slotBackgroundMiddle = PIXI.Sprite.from(blueSlotBackgroundPath);
+slotBackgroundMiddle.anchor.set(0.5, 0.5);
+slotBackgroundMiddle.x = 275;
+slotBackgroundMiddle.y = 350;
+app.stage.addChild(slotBackgroundMiddle);
+
+const slotBackgroundRight = PIXI.Sprite.from(blueSlotBackgroundPath);
+slotBackgroundRight.anchor.set(0.5, 0.5);
+slotBackgroundRight.x = 405;
+slotBackgroundRight.y = 350;
+app.stage.addChild(slotBackgroundRight);
